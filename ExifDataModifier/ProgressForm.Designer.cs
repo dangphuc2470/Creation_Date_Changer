838e7fe6780ed67767946a067c622eba439b1f3d
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-07-25 19:34:24 +0700

    feat: Show geotag progress

---
+++ ExifDataModifier/ProgressForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ExifDataModifier
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            btCancel = new Button();
+            lbProgress = new Label();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 61);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(561, 29);
+            progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Geotagging:";
+            // 
+            // btCancel
+            // 
+            btCancel.Location = new Point(479, 96);
+            btCancel.Name = "btCancel";
+            btCancel.Size = new Size(94, 29);
+            btCancel.TabIndex = 2;
+            btCancel.Text = "Cancel";
+            btCancel.UseVisualStyleBackColor = true;
+            // 
+            // lbProgress
+            // 
+            lbProgress.AutoSize = true;
+            lbProgress.Location = new Point(100, 23);
+            lbProgress.Name = "lbProgress";
+            lbProgress.Size = new Size(50, 20);
+            lbProgress.TabIndex = 3;
+            lbProgress.Text = "label2";
+            // 
+            // ProgressForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(585, 137);
+            Controls.Add(lbProgress);
+            Controls.Add(btCancel);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Name = "ProgressForm";
+            Text = "Geotag";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private Label label1;
+        private Button btCancel;
+        private Label lbProgress;
+    }
+}
---
+++ ExifDataModifier/ProgressForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExifDataModifier
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm()
+    {
+        InitializeComponent();
+    }
+
+    public void UpdateProgress(int progress, string message)
+    {
+        progressBar1.Value = progress;
+        lbProgress.Text = message;
+    }
+    }
+}
