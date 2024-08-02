3c582d06e055b155f69605fdc7813fcbf79c7b72
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-08-02 17:40:18 +0700

    minor: Update README.MD

---
+++ ExifDataModifier/TransparentPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExifDataModifier
+{
+    public class TransparentPanel : Panel
+    {
+        public TransparentPanel()
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = Color.Transparent;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Do not paint background to achieve transparency
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Ensure the panel itself is transparent
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(0, this.BackColor)), this.ClientRectangle);
+            base.OnPaint(e);
+        }
+    }
+
+}
